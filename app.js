833bae5d964e85953da5a6e77a42c83e3cb22a8d
Yogyata123 <yogyatanilkanth@gmail.com>
2016-09-29 18:53:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/',routes.home);
 app.get('/:city',routes.city);
 
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
  var server = app.listen(port,function(request,response){
 
  	console.log("Catch server action at localhost//:" +port)
